71bb82cd8c0248ce6314d3bb7d35fb0b5fcc59e6
iSRHcoder <s.r.hatkadke@gmail.com>
2026-08-18 17:50:19 +0530

    important values are placed in .env for security

--- frontend/utils/firebase.js
+++ frontend/utils/firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { initializeApp } from 'firebase/app';
+import { getAuth, GoogleAuthProvider } from 'firebase/auth';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -8,12 +8,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "multi-agent-ai-49c2f.firebaseapp.com",
-  projectId: "multi-agent-ai-49c2f",
-  storageBucket: "multi-agent-ai-49c2f.firebasestorage.app",
-  messagingSenderId: "306952023199",
-  appId: "1:306952023199:web:7843d85217199afb13c6aa",
-  measurementId: "G-8W94TRTVXG",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
