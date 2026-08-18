d90c0472963015e89f1169c8a45478464f252b41
iSRHcoder <s.r.hatkadke@gmail.com>
2026-08-18 16:27:35 +0530

    Update Redis configuration to support fallback options for improved connection handling
    - Modified Redis initialization to allow for a fallback configuration using host and port environment variables if REDIS_URL is not provided.
    - Enhanced connection reliability by setting maxRetriesPerRequest to null.

--- backend/shared/redis/redis.js
+++ backend/shared/redis/redis.js
@@ -1,6 +1,12 @@
 import Redis from 'ioredis';
 
-const redis = new Redis(process.env.REDIS_URL);
+const redis = new Redis(
+  process.env.REDIS_URL || {
+    host: process.env.REDIS_HOST || '127.0.0.1',
+    port: process.env.REDIS_PORT || 6379,
+    maxRetriesPerRequest: null,
+  }
+);
 
 redis.on('connect', () => {
   console.log('Redis connected');
